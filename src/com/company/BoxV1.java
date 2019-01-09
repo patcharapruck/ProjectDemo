5bea2fb6a0e401b591b7083c711c69c4b4fcde49
Naphatsanan <naphatsanan.boo@msu.ac.th>
2019-01-09 12:52:48 +0700

    Program BMI

--- src/com/company/BoxV1.java
+++ src/com/company/BoxV1.java
@@ -1,10 +1,58 @@
 package com.company;
 
 public class BoxV1 {
-    public double x,y,z;
 
-    public double sum(){
-        return x+y+z;
+    private String Name;
+    private Long Id;
+    private Double weight;
+    private Double height;
+    private String address;
+    private Double result;
+
+    public String getName() {
+        return Name;
     }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+public Double BMI(){
+   result = weight / Math.pow((height*0.01),2);
+        return result;
+}
+
+
 }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 	    BoxV1 boxV1 = new BoxV1();
-	    boxV1.x = 10;
-	    boxV1.y = 20;
-	    boxV1.z = 29.7;
+	   boxV1.setName("Naphatsanan Boonchom");
+	   boxV1.setId(58011212107L);
+	   boxV1.setAddress("House No.54 Village No. 18 Namkham Village " +
+			   "Namkham Sub-district Thatphanom district Nakhonphanom Province " +
+			   "Postal Code 48110");
+	   boxV1.setHeight(165.0);
+	   boxV1.setWeight(60.2);
+	   System.out.println( boxV1.BMI());
 
-	    System.out.println(boxV1.sum());
+
+
+
     }
 }
