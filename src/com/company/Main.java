42afbfa92290d899ef19066559315e3b030d1f65
PCHRP <fluk.fluk.zuza@gmail.com>
2019-01-09 11:39:18 +0700

    แก้ไข ค่าตัวแปร y

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	    BoxV1 boxV1 = new BoxV1();
 	    boxV1.x = 10;
-	    boxV1.y = 20.2;
+	    boxV1.y = 20;
 	    boxV1.z = 29.7;
 
 	    System.out.println(boxV1.sum());
