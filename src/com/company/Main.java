baddeb80c5727b7bfe3196d805ca421dd046d862
Naphatsanan <naphatsanan.boo@msu.ac.th>
2019-01-09 13:00:47 +0700

    Program BMI

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+    	Double bmi;
 	    BoxV1 boxV1 = new BoxV1();
 	   boxV1.setName("Naphatsanan Boonchom");
 	   boxV1.setId(58011212107L);
@@ -11,7 +12,25 @@ public class Main {
 			   "Postal Code 48110");
 	   boxV1.setHeight(165.0);
 	   boxV1.setWeight(60.2);
-	   System.out.println( boxV1.BMI());
+	   bmi = boxV1.BMI();
+
+	   System.out.println(bmi);
+
+	   if (bmi<=18.5){
+		   System.out.println("You thin");
+	   }
+		else if (bmi<=22.9){
+			System.out.println("You Good");
+		}
+	   else if (bmi<=24.9){
+		   System.out.println("You Over weight");
+	   }
+	   else if (bmi<=29.9){
+		   System.out.println("You Fat");
+	   }
+	   else{
+		   System.out.println("You Vary Fat");
+	   }
 
 
 
